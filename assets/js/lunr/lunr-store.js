f463cc8fe99c4842c386787eea02bfc35e7fe892
avadae <alex.vanden.abeele@howest.be>
2025-08-01 17:07:41 +0000

    Deploying to gh-pages from @ avadae/game-groupprojects@e72db639a25075f1edce7449891f181bb039befb 🚀

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,19 @@
+var store = [{
+        "title": "Otterworldly",
+        "excerpt":"The Province of West Flanders organized three editions of the Festival of the Future between 2021 and 2024. On the occasion of the festive opening of the renovated Provincial Court, the last edition of “a festival of the future” called ‘Over-Morgen’ took place. The challenge for the students was to...","categories": [],
+        "tags": [],
+        "url": "/game-groupprojects/2023/12/15/mechanimals.html",
+        "teaser": "/game-groupprojects/assets/images/mechanimals/mech01.webp"
+      },{
+        "title": "OTIV",
+        "excerpt":"OTIV is a company that focuses on creating assistance systems within the rail industry (trains and trams). In this project we’ve build a virtual world in which tram AI algorithms can train their models. The goal was to replicate a real city layout. Based upon data from Open Street Map...","categories": [],
+        "tags": [],
+        "url": "/game-groupprojects/2023/12/15/otiv.html",
+        "teaser": "/game-groupprojects/assets/images/otiv.webp"
+      },{
+        "title": "Fire @ Home",
+        "excerpt":"Hulpverleningszone Fluvia is the local fire department here in Kortrijk. As a public fire service they have various tasks towards the community. Bringing safety messages and teaching children how to act in case of fire is one of those tasks. Last year we already partnered with Fluvia and created the...","categories": [],
+        "tags": [],
+        "url": "/game-groupprojects/2024/12/15/fluvia.html",
+        "teaser": "/game-groupprojects/assets/images/fire/fluvia_02.webp"
+      }]
---
+++ nl/assets/js/lunr/lunr-store.js
@@ -0,0 +1,19 @@
+var store = [{
+        "title": "Otterworldly",
+        "excerpt":"De Provincie West-Vlaanderen organiseerde tussen 2021 en 2024 drie edities van het Festival van de Toekomst. Ter gelegenheid van de feestelijke opening van het gerenoveerde Provinciaal Hof vond de laatste editie van “een festival van de toekomst” plaats: ‘Over-Morgen’. De uitdaging voor de studenten was om zich voor te stellen...","categories": [],
+        "tags": [],
+        "url": "/game-groupprojects/2023/12/15/mechanimals.html",
+        "teaser": "/game-groupprojects/assets/images/mechanimals/mech01.webp"
+      },{
+        "title": "OTIV",
+        "excerpt":"OTIV is een bedrijf dat zich richt op het ontwikkelen van assistentiesystemen binnen de spoorwegsector (treinen en trams). In dit project hebben we een virtuele wereld gebouwd waarin AI-algoritmen voor trams hun modellen kunnen trainen. Het doel was om een echte stadsplattegrond na te bootsen. Op basis van gegevens van...","categories": [],
+        "tags": [],
+        "url": "/game-groupprojects/2023/12/15/otiv.html",
+        "teaser": "/game-groupprojects/assets/images/otiv.webp"
+      },{
+        "title": "Fire @ Home",
+        "excerpt":"Hulpverleningszone Fluvia is de lokale brandweer van Kortrijk. Als openbare brandweer hebben ze verschillende taken voor de gemeenschap. Veiligheidsinstructies geven en kinderen leren hoe ze moeten handelen bij brand is daar één van. Vorig jaar werkten we al samen met Fluvia en ontwikkelden we het spel “Fire Escape”, dat nu...","categories": [],
+        "tags": [],
+        "url": "/game-groupprojects/2024/12/15/fluvia.html",
+        "teaser": "/game-groupprojects/assets/images/fire/fluvia_02.webp"
+      }]
